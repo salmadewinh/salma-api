2fc5646f7f40e40b6db2a2d11a43156185dcd6c2
it'sme <94110881+salmadewinh@users.noreply.github.com>
2022-07-03 21:18:49 -0700

    Update database.js

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,7 +1,7 @@
 module.exports = {
     multipleStatements: true,
     host: 'db4free.net',
-    user: 'hwangyeji',
-    password: 'Einshearth1111',
-    database: 'pkl_ay'
+    user: 'salmadb',
+    password: '12082002',
+    database: 'dbhandphone'
 };
